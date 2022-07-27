d51b1cf4a524e1bb63ad6449c506294e3d534163
Guilherme Viana <gguicido.viana@gmail.com>
2022-07-26 23:09:29 -0300

    metodo para cadastro de sistemas

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -84,8 +84,6 @@
             </div>
         </div>
     </section>
-    <!-- /.card -->
-
     <!--data table sectin-->
     <section class="content">
         <div class="card">
@@ -94,7 +92,6 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Resultado da pesquisa de clientes</h3>
                                 <div class="card-tools">
                                     <div class="input-group input-group-sm">
                                         <div class="dt-buttons btn-group flex-wrap">
@@ -109,14 +106,15 @@
                                 </div>
                             </div>
                             <div class="card-body table-responsive p-0">
-                                <table class="table table-hover">
-                                    <thead>
+                                <table class="table table-hover  table-sm">
+                                    <thead class="bg-light">
                                         <tr>
                                             <th>#</th>
                                             <th>Nome Cliente</th>
-                                            <th>Data Cadastro</th>
-                                            <th>CPF / CNPJ</th>
+                                            <th>Data</th>
+                                            <th>CPF</th>
                                             <th>Email</th>
+                                            <th>Cidade</th>
                                             <th>Situação</th>
                                             <th>Ações</th>
                                         </tr>
@@ -124,17 +122,18 @@
                                     <tbody>
                                         <tr>
                                             @if ($clientes->isEmpty())
-                                                <td colspan="7"> Nenhum Resultado encontrado </td>
+                                                <td colspan="8"> Nenhum Resultado encontrado </td>
                                             @else
                                             @endif
                                             @foreach ($clientes as $cliente)
-                                                <th scope="row dark">{{ $cliente->id }}</th>
+                                                <th scope="row info">{{ $cliente->id }}</th>
                                                 <td>{{ $cliente->nome_cliente }}</td>
                                                 <td style="text-align: center">
                                                     {{ date('Y/m/d', strtotime($cliente->data_cadastro)) }}</td>
                                                 <td>{{ $cliente->cpf_cliente }}</td>
                                                 <td>{{ $cliente->email_cliente }}</td>
-                                                <td>{{ $cliente->Situacao->situacao }}</td>
+                                                <td>{{ $cliente->cidade_endereco }}</td>
+                                                <td >{{ $cliente->Situacao->situacao }}</td>
                                                 <td id="opcoes">
                                                     <button id="search"
                                                         onclick="showModal('{{ route('cliente.show', $cliente->id) }}', 'Detalhes Cliente {{ $cliente->nome }}', ' Cliente', 'Salvar', 'Cancelar');"
@@ -152,34 +151,33 @@
                                                                 class="fas fa-fw fa-trash"></i></button>
                                                     </form>
                                                 </td>
-                                        </tr>
-                                        @endforeach
-                                        <tr>
-
-                                            {{-- <td colspan=7>     
-                                            <div>
-                                                <ul class="pagination float-right">
-                                                  <li class="page-item disabled">
-                                                    <a class="page-link" href="{{ $clientes->previousPageUrl() }}">&laquo;</a>
-                                                  </li>
-                                                  @for ($i = 1; $i <= $clientes->lastPage(); $i++)
-                                                  <li class="page-item">
-                                                    <a class="page-link" href="{{ $clientes->url($i) }}">{{ $i }}</a>
-                                                  </li>
-                                                  <li class="page-item">
-                                                    <a href="{{ $clientes ->nextPageUrl() }}">
-                                                  </li>
-                                                  @endfor
-                                                  <li class="page-item">
-                                                    <a class="page-link" href="{{ $clientes ->nextPageUrl() }}">&raquo;</a>
-                                                  </li>
-                                                </ul>
-                                              </div>
-                                        </td> --}}
-                                        </tr>
+                                            </tr>
+                                            @endforeach
                                     </tbody>
                                 </table>
+                            
 
+                                    {{-- <td colspan=7>     
+                                    <div>
+                                        <ul class="pagination float-right">
+                                          <li class="page-item disabled">
+                                            <a class="page-link" href="{{ $clientes->previousPageUrl() }}">&laquo;</a>
+                                          </li>
+                                          @for ($i = 1; $i <= $clientes->lastPage(); $i++)
+                                          <li class="page-item">
+                                            <a class="page-link" href="{{ $clientes->url($i) }}">{{ $i }}</a>
+                                          </li>
+                                          <li class="page-item">
+                                            <a href="{{ $clientes ->nextPageUrl() }}">
+                                          </li>
+                                          @endfor
+                                          <li class="page-item">
+                                            <a class="page-link" href="{{ $clientes ->nextPageUrl() }}">&raquo;</a>
+                                          </li>
+                                        </ul>
+                                      </div>
+                                </td> --}}
+                             
                             </div>
                         </div>
                     </div>
