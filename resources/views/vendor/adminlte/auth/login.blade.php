a9d27dd6a2b59b9b8b6781410eec5e36247c0383
Guilherme Viana <gguicido.viana@gmail.com>
2022-08-06 15:22:19 -0300

    mudanças na tela de login

--- resources/views/vendor/adminlte/auth/login.blade.php
+++ resources/views/vendor/adminlte/auth/login.blade.php
@@ -21,66 +21,69 @@
 @section('auth_header', __('adminlte::adminlte.login_message'))
 
 @section('auth_body')
-    <form action="{{ $login_url }}" method="post">
-        @csrf
+    <div class="login-box">
+        <h2>Administrativo</h2>
+        <form action="{{ $login_url }}" method="post">
+            @csrf
 
-        {{-- Email field --}}
-        <div class="input-group mb-3">
-            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
-                   value="{{ old('email') }}" placeholder="{{ __('adminlte::adminlte.email') }}" autofocus>
+            {{-- Email field --}}
+            <div class="input-group mb-3">
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
+                       value="{{ old('email') }}" placeholder="{{ __('adminlte::adminlte.email') }}" autofocus>
 
-            <div class="input-group-append">
-                <div class="input-group-text">
-                    <span class="fas fa-envelope {{ config('adminlte.classes_auth_icon', '') }}"></span>
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-envelope {{ config('adminlte.classes_auth_icon', '') }}"></span>
+                    </div>
                 </div>
-            </div>
 
-            @error('email')
+                @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
-            @enderror
-        </div>
-
-        {{-- Password field --}}
-        <div class="input-group mb-3">
-            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
-                   placeholder="{{ __('adminlte::adminlte.password') }}">
-
-            <div class="input-group-append">
-                <div class="input-group-text">
-                    <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
-                </div>
+                @enderror
             </div>
 
-            @error('password')
+            {{-- Password field --}}
+            <div class="input-group mb-3">
+                <input type="password" name="password" class="form-control  @error('password') is-invalid @enderror"
+                       placeholder="{{ __('adminlte::adminlte.password') }}">
+
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
+                    </div>
+                </div>
+
+                @error('password')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
-            @enderror
-        </div>
+                @enderror
+            </div>
 
-        {{-- Login field --}}
-        <div class="row">
-            <div class="col-7">
-                <div class="icheck-dark" title="{{ __('adminlte::adminlte.remember_me_hint') }}">
-                    <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+            {{-- Login field --}}
+            <div class="row">
+                <div class="col-7">
+                    <div class="icheck-dark" title="{{ __('adminlte::adminlte.remember_me_hint') }}">
+                        <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                    <label for="remember">
-                        {{ __('adminlte::adminlte.remember_me') }}
-                    </label>
+                        <label style="color: #fff" for="remember">
+                            {{ __('adminlte::adminlte.remember_me') }}
+                        </label>
+                    </div>
+                </div>
+
+                <div class="col-5">
+                    <button type=submit class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-dark') }}">
+                        <span class="fas fa-sign-in-alt"></span>
+                        {{ __('adminlte::adminlte.sign_in') }}
+                    </button>
                 </div>
             </div>
 
-            <div class="col-5">
-                <button type=submit class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-dark') }}">
-                    <span class="fas fa-sign-in-alt"></span>
-                    {{ __('adminlte::adminlte.sign_in') }}
-                </button>
-            </div>
-        </div>
-
-    </form>
+        </form>
+    </div>
 @stop
 
 @section('auth_footer')
